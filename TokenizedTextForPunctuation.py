cb82fd60f4eddffd935f99134e31ca5dad082d10
joe-ufa <34921037+joe-ufa@users.noreply.github.com>
2018-02-16 10:57:06 -0500

    Add files via upload

--- TokenizedTextForPunctuation.py
+++ TokenizedTextForPunctuation.py
@@ -100,33 +100,34 @@ class TokenizedText:
     # Class TokenizedText
     # Takes raw text and inserts '<stop>' where an inferred period should be.
     def insert_stops(self, text):
+        text_with_stops = ''
         i = 0
         while i < len(text):
+            stop_found = False
             if ((text[i] in self.WHITE_SPACE)):
-                prefix_word_list = [re.sub(self.NUM_REGEX, "<NUM>", word) for word in text[:i].split()]
-                if (len(prefix_word_list) >= 2) and ((re.sub(self.PUNCTUATION_REGEX, "", prefix_word_list[0]), re.sub(self.PUNCTUATION_REGEX, "", prefix_word_list[1])) in self.sentence_begin_bigrams):
-                    return '<stop>' + text[:i+1] + self.insert_stops(text[i+1:])
-                if (len(prefix_word_list) >= 2) and ((re.sub(self.PUNCTUATION_REGEX, "", prefix_word_list[-2]), re.sub(self.PUNCTUATION_REGEX, "", prefix_word_list[-1])) in self.sentence_end_bigrams):
-                    return text[:i] + '<stop>' + self.insert_stops(text[i:])
+                word_list = [re.sub(self.NUM_REGEX, "<NUM>", word) for word in text[i+1:].split()]
+                if (len(word_list) >= 2) and ((re.sub(self.PUNCTUATION_REGEX, "", word_list[0]), re.sub(self.PUNCTUATION_REGEX, "", word_list[1])) in self.sentence_begin_bigrams):
+                    text_with_stops = text_with_stops + text[i] + '<stop>'
+                    stop_found = True
+                word_list = [re.sub(self.NUM_REGEX, "<NUM>", word) for word in text[:i].split()]
+                if (len(word_list) >= 2) and ((re.sub(self.PUNCTUATION_REGEX, "", word_list[-2]), re.sub(self.PUNCTUATION_REGEX, "", word_list[-1])) in self.sentence_end_bigrams):
+                    text_with_stops = text_with_stops + '<stop>' + text[i]
+                    stop_found = True
+            if not stop_found:
+                text_with_stops = text_with_stops + text[i]
             i = i + 1
-        return text             # Recursion base case.
+        return text_with_stops
 
 
     # Class TokenizedText
     # Takes a sentence as input and returns a list of sentences after inserting inferred periods.
-    def split_into_sentences_with_inferred_punctuation(self, text):
-        return self.split_into_sentences(self.insert_stops(text), False)
+    def split_into_sentences_with_inferred_punctuation(self, text, treat_comma_as_period):
+        return self.split_into_sentences(self.insert_stops(text), treat_comma_as_period)
     
 
     # Class TokenizedText
     # Populates the TokenizedText object for the document in the file filename.
     def populate_obj(self, text, treat_comma_as_period):
-        text = text.replace("\\n"," \n")         # Substitute newline characters for raw '\n', using spaces to maintain offset counts.
-        text = text.replace(r"\\.br\\", "      \n")   # Substitute newline characters for these strings, using spaces to maintain offset counts.
-        self.whole_doc = text
-        self.paragraphs = [(para + self.PARA_DELIMITER) for para in text.split(self.PARA_DELIMITER)]
-        self.paragraph_sentences = [self.split_into_sentences(para, treat_comma_as_period) for para in self.paragraphs]
-        self.paragraph_sentences_with_inferred_punctuation = [self.split_into_sentences_with_inferred_punctuation(para) for para in self.paragraphs]
         with open(self.SENTENCE_BEGIN_BIGRAMS_FILE, 'rb') as csvfile:
             bigram_reader = csv.reader(csvfile, delimiter = self.CSV_SEPARATOR)
             for row in bigram_reader:
@@ -135,6 +136,12 @@ class TokenizedText:
             bigram_reader = csv.reader(csvfile, delimiter = self.CSV_SEPARATOR)
             for row in bigram_reader:
                 self.sentence_end_bigrams.append((row[0], row[1]))
+        text = text.replace("\\n"," \n")         # Substitute newline characters for raw '\n', using spaces to maintain offset counts.
+        text = text.replace(r"\\.br\\", "      \n")   # Substitute newline characters for these strings, using spaces to maintain offset counts.
+        self.whole_doc = text
+        self.paragraphs = [(para + self.PARA_DELIMITER) for para in text.split(self.PARA_DELIMITER)]
+        self.paragraph_sentences = [self.split_into_sentences(para, treat_comma_as_period) for para in self.paragraphs]
+        self.paragraph_sentences_with_inferred_punctuation = [self.split_into_sentences_with_inferred_punctuation(para, treat_comma_as_period) for para in self.paragraphs]
 
 
  
@@ -142,7 +149,10 @@ class TokenizedText:
 
 # Test loop.
 #s = " The quick  red  fox  jumped over the lazy brown dog, maybe.  Maybe not!"
-s = "She is an older male with prostate cancer and who knows what else."
+s = "She is a man, ahem, with prostate cancer and other problems."
 tt = TokenizedText()
-tt.populate_obj(s, False)
-print tt.insert_stops(s)
+tt.populate_obj(s, True)
+print tt.paragraphs
+print tt.paragraph_sentences
+print tt.paragraph_sentences_with_inferred_punctuation
+
